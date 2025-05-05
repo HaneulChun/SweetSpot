5fd72f19090d75869ff04fb2544e37d2cbae4ec3
Jason Saltenis <pg27jason@vfs.com>
2025-05-05 11:37:32 -0700

    Added Protos?

---
+++ Source/UnrealProjectBase/PlayerHud.h
@@ -0,0 +1,52 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/HUD.h" 
+#include "PlayerHud.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class UNREALPROJECTBASE_API APlayerHud : public AHUD
+{
+	GENERATED_BODY()
+
+protected:
+	FTimerHandle TimerHandle;
+	
+	virtual void BeginPlay() override;
+
+	void UpdateTimer();
+
+	virtual void DrawHUD() override;
+	
+public:
+	APlayerHud();
+	
+	UPROPERTY(EditAnywhere, BluePrintReadWrite)
+	float Timer = 360;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "UI", meta = (AllowPrivateAccess = "true"))
+	TSubclassOf<UUserWidget> WidgetClass;
+
+	UPROPERTY(BlueprintReadWrite, Category = "UI", meta = (AllowPrivateAccess = "true"))
+	UUserWidget* CurrentWidget;
+	
+	UFUNCTION(BlueprintCallable, Category = "UI")
+	UUserWidget* GetWidget() const;
+
+	UPROPERTY(EditAnywhere, BluePrintReadWrite)
+	float sweatSpot = 0.4;
+
+	UPROPERTY(EditAnywhere, BluePrintReadWrite)
+	float mad = 0.6;
+	
+	UPROPERTY(EditAnywhere, BluePrintReadWrite)
+	TArray<UMaterialInterface*> Material;
+private:
+	FString timerText = "";
+	float CurrentTimer;
+};
