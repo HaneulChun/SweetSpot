2880148df2455e4b7b57806bc5343ba9833e4878
Jason Saltenis <pg27jason@vfs.com>
2025-05-15 14:07:39 -0700

    Temp commented out battery madness

--- Source/UnrealProjectBase/Wristwatch/Public/BatteryMadness.h
+++ Source/UnrealProjectBase/Wristwatch/Public/BatteryMadness.h
@@ -1,35 +1,35 @@
-#pragma once
-
-#include "CoreMinimal.h"
-#include "Engine/TextRenderActor.h"
-#include "BatteryMadness.generated.h"
-
-UCLASS()
-class UNREALPROJECTBASE_API ABatteryMadness : public ATextRenderActor
-{
-	GENERATED_BODY()
-
-public:
-	ABatteryMadness();
-
-protected:
-	virtual void BeginPlay() override;
-	virtual void Tick(float DeltaTime) override;
-
-private:
-	// UMG widget class reference (set in editor)
-	UPROPERTY(EditAnywhere, Category = "UI")
-	TSubclassOf<class UUserWidget> BatteryContainerClass;
-
-	// Widget instance and casted reference
-	UPROPERTY()
-	class UMadnessMeter* MadnessWidget;
-
-	// Battery UI text updater
-	void UpdateBatteryText();
-
-	UPROPERTY(EditAnywhere, Category = "UI")
-	float UpdateInterval = 0.25f;
-
-	float TimeSinceLastUpdate = 0.0f;
-};
+// #pragma once
+//
+// #include "CoreMinimal.h"
+// #include "Engine/TextRenderActor.h"
+// #include "BatteryMadness.generated.h"
+//
+// UCLASS()
+// class UNREALPROJECTBASE_API ABatteryMadness : public ATextRenderActor
+// {
+// 	GENERATED_BODY()
+//
+// public:
+// 	ABatteryMadness();
+//
+// protected:
+// 	virtual void BeginPlay() override;
+// 	virtual void Tick(float DeltaTime) override;
+//
+// private:
+// 	// UMG widget class reference (set in editor)
+// 	UPROPERTY(EditAnywhere, Category = "UI")
+// 	TSubclassOf<class UUserWidget> BatteryContainerClass;
+//
+// 	// Widget instance and casted reference
+// 	UPROPERTY()
+// 	class UMadnessMeter* MadnessWidget;
+//
+// 	// Battery UI text updater
+// 	void UpdateBatteryText();
+//
+// 	UPROPERTY(EditAnywhere, Category = "UI")
+// 	float UpdateInterval = 0.25f;
+//
+// 	float TimeSinceLastUpdate = 0.0f;
+// };
