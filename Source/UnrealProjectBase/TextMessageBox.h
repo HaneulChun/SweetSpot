01b1828932c37a1c19edb10d80318cb4cd02af3b
Jason Saltenis <pg27jason@vfs.com>
2025-06-17 13:37:01 -0700

    added fonts

---
+++ Source/UnrealProjectBase/TextMessageBox.h
@@ -0,0 +1,17 @@
+// All Rights Reserved by SweetSpot 2025-2026.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Components/WidgetComponent.h"
+#include "TextMessageBox.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class UNREALPROJECTBASE_API UTextMessageBox : public UWidgetComponent
+{
+	GENERATED_BODY()
+	
+};
