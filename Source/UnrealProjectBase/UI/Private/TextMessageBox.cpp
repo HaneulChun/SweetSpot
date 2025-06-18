ae336ebe9a23fe0c4fede709b56b05201d9b8a5f
Jason Saltenis <pg27jason@vfs.com>
2025-06-17 18:31:57 -0700

    Fixed eyes open anim

--- Source/UnrealProjectBase/UI/Private/TextMessageBox.cpp
+++ Source/UnrealProjectBase/UI/Private/TextMessageBox.cpp
@@ -1,31 +1,31 @@
-// // All Rights Reserved by SweetSpot 2025-2026.
-//
-//
-// #include "UnrealProjectBase/UI/Public/TextMessageBox.h"
-// #include "Blueprint/UserWidget.h"
-// #include "Components/TextBlock.h"
-//
-// UATextMessageBox::UATextMessageBox() : WBP_MessageBox_Container(nullptr), Text_MessageBox(nullptr)
-// {
-// }
-//
-// void UATextMessageBox::NativeConstruct()
-// {
-// 	Super::NativeConstruct();
-//
-// 	if (!WBP_MessageBox_Container)
-// 	{
-// 	}
-// }
-//
-// void UATextMessageBox::NativeTick(float DeltaTime)
-// {
-// 	Super::bAutomaticallyRegisterInputOnConstruction = false;
-//
-// 	if (Text_MessageBox)
-// 	{
-// 		std::string const PushedPlayerMessage = "<Message>";
-// 		FString MessageBox_Text = "PushedPlayerMessage";
-// 		Text_MessageBox->SetText(FText::FromString(MessageBox_Text));
-// 	}
-// }
+// All Rights Reserved by SweetSpot 2025-2026.
+
+
+#include "UnrealProjectBase/UI/Public/TextMessageBox.h"
+#include "Blueprint/UserWidget.h"
+#include "Components/TextBlock.h"
+
+UATextMessageBox::UATextMessageBox() : WBP_MessageBox_Container(nullptr), Text_MessageBox(nullptr)
+{
+}
+
+void UATextMessageBox::NativeConstruct()
+{
+	Super::NativeConstruct();
+
+	if (!WBP_MessageBox_Container)
+	{
+	}
+}
+
+void UATextMessageBox::NativeTick(float DeltaTime)
+{
+	NativeTick(DeltaTime);
+
+	if (Text_MessageBox)
+	{
+		std::string const PushedPlayerMessage = "<Message>";
+		FString MessageBox_Text = "PushedPlayerMessage";
+		Text_MessageBox->SetText(FText::FromString(MessageBox_Text));
+	}
+}
--- Source/UnrealProjectBase/UI/Public/TextMessageBox.h
+++ Source/UnrealProjectBase/UI/Public/TextMessageBox.h
@@ -1,40 +1,40 @@
-// // All Rights Reserved by SweetSpot 2025-2026.
-//
-// #pragma once
-//
-// #include "CoreMinimal.h"
-// #include "Blueprint/UserWidget.h"
-// #include "TextMessageBox.generated.h"
-//
-// /**
-//  * 
-//  */
-// class UTextBlock;
-// class UUserWidget;
-//
-// UCLASS()
-// class UNREALPROJECTBASE_API UATextMessageBox : public UUserWidget
-// {
-// 	GENERATED_BODY()
-//
-// 	
-// public:	
-// 	// Sets default values for this actor's properties
-// 	UATextMessageBox();
-//
-// protected:
-// 	// Called when the game starts or when spawned
-// 	virtual void NativeConstruct() override;
-//
-// public:	
-// 	// Called every frame
-// 	virtual void NativeTick(float DeltaTime);
-//
-// 	// Reference to the desired widget
-// 	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "UI")
-// 	UUserWidget* WBP_MessageBox_Container;
-//
-// 	// Reference to the desired text block
-// 	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "UI")
-// 	UTextBlock* Text_MessageBox;
-// };
+// All Rights Reserved by SweetSpot 2025-2026.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Blueprint/UserWidget.h"
+#include "TextMessageBox.generated.h"
+
+/**
+ * 
+ */
+class UTextBlock;
+class UUserWidget;
+
+UCLASS()
+class UNREALPROJECTBASE_API UATextMessageBox : public UUserWidget
+{
+	GENERATED_BODY()
+
+	
+public:	
+	// Sets default values for this actor's properties
+	UATextMessageBox();
+
+protected:
+	// Called when the game starts or when spawned
+	virtual void NativeConstruct() override;
+
+public:	
+	// Called every frame
+	virtual void NativeTick(float DeltaTime);
+
+	// Reference to the desired widget
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "UI")
+	UUserWidget* WBP_MessageBox_Container;
+
+	// Reference to the desired text block
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "UI")
+	UTextBlock* Text_MessageBox;
+};
