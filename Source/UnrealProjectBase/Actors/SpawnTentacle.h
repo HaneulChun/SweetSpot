27dba9678a827f749e7a57cb58f6a75aacfb5d61
Haneul Chun <pg27haneul@vfs.com>
2025-06-25 09:31:21 -0700

    started spawning tentacle

---
+++ Source/UnrealProjectBase/Actors/SpawnTentacle.h
@@ -0,0 +1,30 @@
+// All Rights Reserved by SweetSpot 2025-2026.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Actor.h"
+#include "SpawnTentacle.generated.h"
+
+class UArrowComponent;
+class UBoxComponent;
+
+UCLASS()
+class UNREALPROJECTBASE_API ASpawnTentacle : public AActor
+{
+	GENERATED_BODY()
+	
+public:	
+	// Sets default values for this actor's properties
+	ASpawnTentacle();
+
+protected:
+	// Called when the game starts or when spawned
+	virtual void BeginPlay() override;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite);
+	TObjectPtr<UBoxComponent> triggerBox;
+public:	
+	UPROPERTY(EditAnywhere, BlueprintReadWrite)
+	TArray<TObjectPtr<UArrowComponent>> ArrowArray;
+};
