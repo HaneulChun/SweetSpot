a3663e5addc5967fe2f0338b35e19a7afb3c754a
Haneul Chun <pg27haneul@vfs.com>
2025-07-15 17:22:34 -0700

    fixed tv interact trigger box

--- Source/UnrealProjectBase/Actors/Room.cpp
+++ Source/UnrealProjectBase/Actors/Room.cpp
@@ -152,20 +152,20 @@ void ARoom::Color(float intensity, float Vignette)
 {
 	if (APlayerController* PlayerController = GetWorld()->GetFirstPlayerController())
 	{
-			FPostProcessSettings& Settings = Camera->PostProcessSettings;
+		FPostProcessSettings& Settings = Camera->PostProcessSettings;
 
-			if (intensity <= 1)
-			{
-				Settings.bOverride_ColorSaturation = true;
-				Settings.ColorSaturation = FVector4(intensity, intensity, intensity, 1.0f);
+		if (intensity <= 1)
+		{
+			Settings.bOverride_ColorSaturation = true;
+			Settings.ColorSaturation = FVector4(intensity, intensity, intensity, 1.0f);
 
-				Settings.bOverride_VignetteIntensity = true;
-				Settings.VignetteIntensity = Vignette;	
-			}
-			else
-			{
-				Settings.bOverride_ColorSaturation = true;
-				Settings.ColorSaturation = FVector4(intensity, intensity/2, intensity, 1.0f);
-			}
+			Settings.bOverride_VignetteIntensity = true;
+			Settings.VignetteIntensity = Vignette;	
+		}
+		else
+		{
+			Settings.bOverride_ColorSaturation = true;
+			Settings.ColorSaturation = FVector4(intensity, intensity/2, intensity, 1.0f);
+		}
 	}
 }
