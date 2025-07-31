16d729172c9fbabf9591dc92ac0211cfa571ed0b
Haneul Chun <pg27haneul@vfs.com>
2025-07-31 13:32:05 -0700

    fixed tentacles coming back up

--- Source/UnrealProjectBase/Actors/TentacleWall.cpp
+++ Source/UnrealProjectBase/Actors/TentacleWall.cpp
@@ -61,7 +61,6 @@ void ATentacleWall::Tick(float DeltaTime)
 	// tentacle fading up
 	currentAlpha += comingUpSpeed * DeltaTime;
 	FVector NewLocation = FMath::Lerp(finalLocation, startLocation, currentAlpha);
-	//SetActorLocation(NewLocation);
 	MovingSceneComponent->SetWorldLocation(NewLocation);
 		
 	if (currentAlpha >= 1)
@@ -122,10 +121,8 @@ void ATentacleWall::FadeAway()
 	{
 		// make eye disappear 
 		FVector Direction = -GetActorUpVector();
-		//FVector CurrentLocation = GetActorLocation();
 		FVector CurrentLocation = MovingSceneComponent->GetComponentLocation();
 		FVector NewLocation = CurrentLocation + (Direction * speed);
-		//SetActorLocation(NewLocation);
 		MovingSceneComponent->SetWorldLocation(NewLocation);
 
 		CameraShake();
@@ -148,14 +145,13 @@ void ATentacleWall::Spawn()
 
 void ATentacleWall::StartDown()
 {
-	//SetActorLocation(finalLocation);
 	MovingSceneComponent->SetWorldLocation(finalLocation);
 }
 
 void ATentacleWall::ResetTentaclePosition_Implementation()
 {
 	ResetTentaclePosition();
-	finalLocation = GetActorLocation();
+	finalLocation = MovingSceneComponent->GetComponentLocation();
 	currentAlpha = 0;
 	SetActorTickEnabled(true);
 	count = 0;
