3a4132ea978fc0945b0d37dd63ca359acf2863f4
Haneul Chun <pg27haneul@vfs.com>
2025-07-31 17:20:29 -0700

    Vignette for when the black void gets close

--- Source/UnrealProjectBase/Actors/BlackVoid.cpp
+++ Source/UnrealProjectBase/Actors/BlackVoid.cpp
@@ -4,8 +4,10 @@
 #include "BlackVoid.h"
 
 #include "SweetSpotCharacter.h"
+#include "VectorTypes.h"
 #include "Components/BillboardComponent.h"
 #include "Components/BoxComponent.h"
+#include "UnrealProjectBase/Component/CameraSettingsComponent.h"
 #include "UnrealProjectBase/UI/PlayerHud.h"
 
 // Sets default values
@@ -52,6 +54,7 @@ void ABlackVoid::BeginPlay()
 	SetActorTickEnabled(false);
 
 	player = Cast<ASweetSpotCharacter>(GetWorld()->GetFirstPlayerController()->GetPawn());
+	CameraSettings = GetWorld()->GetFirstPlayerController()->GetPawn()->FindComponentByClass<UCameraSettingsComponent>();
 }
 
 // Called every frame
@@ -65,7 +68,26 @@ void ABlackVoid::Tick(float DeltaTime)
 		float Alpha = FMath::Clamp(ElapsedTime / MoveDuration, 0.0f, 1.0f);
 		FVector NewLocation = FMath::Lerp(start->GetComponentLocation(), end->GetComponentLocation(), Alpha);
 		moveingComponent->SetWorldLocation(NewLocation);
-		float distance = GetDistanceTo(player);
+		//float distance = GetDistanceTo(player);
+		float distance = UE::Geometry::Distance(moveingComponent->GetComponentLocation(), player->GetActorLocation());
+
+		// very clsoe
+		if (distance < 500 && DistanceState != EDistance::veryClose)
+		{
+			CameraSettings->SetCameraSettings(1, 3, 90);
+			DistanceState = EDistance::close;
+		}
+		// close
+		else if (distance < 1000 && DistanceState != EDistance::close)
+		{
+			CameraSettings->SetCameraSettings(1, 2, 90);
+			DistanceState = EDistance::close;
+		}
+		else if (distance < 1500 && DistanceState != EDistance::far)
+		{
+			CameraSettings->SetCameraSettings(1, 1, 90);
+			DistanceState = EDistance::far;
+		}
 	}
 	else
 	{
